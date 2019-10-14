737df341154fc629e44d9cb6c07e52a71dea1d8c
Charlles <charlleslimamota10@hotmail.com>
2019-10-14 00:56:27 -0300

    Adicionar arquivos de projeto.

---
+++ FunWithMaps/FunWithMaps/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Android;
+using Android.Support.V4.App;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace FunWithMaps
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {      
+    public MainPage()
+        {
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                        new Position(37, -122), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+        }
+        
+    }
+}
